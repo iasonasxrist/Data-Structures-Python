46e06097e56f8995fd810ef98064d16c6d2d70cb
iasonasxrist <iasonasxrist@gmail.com>
2024-04-15 19:06:48 +0300

    Remove

--- Array/Leetcode.py
+++ Array/Leetcode.py
@@ -85,3 +85,36 @@ def getConcatenation(nums):
 nums = [1,2,3]
 new = getConcatenation(nums)
 print(new)
+
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+
+        left, right = 0 , len(nums)-1
+
+        while left <= right:
+            mid = left + (right - left) //2
+            if nums[mid] == target:
+                return True
+            if nums[left] == nums[mid]:
+                left+=1
+                continue
+            if  nums[mid] < nums[right]:
+                if nums[mid] < target <= nums[right]: 
+                    left = mid+1
+                else:
+                   right = mid-1
+            else:
+                if nums[left] <= target <= nums[mid]:
+
+                    right= mid - 1
+                else:
+                    left = mid + 1
+        return False
+
+solution = Solution()
+ok = solution.search([1,1,1,1,1,1,1,1,1,13,1,1,1,1,1,1,1,1,1,1,1,1], 13)
+print("ok",ok) 
+
+
+
